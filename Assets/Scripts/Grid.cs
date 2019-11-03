474a843615a061c6f524ae880f14f84da995e882
j-alizadeh <jalizade3d@yahoo.com>
2019-11-03 22:41:09 +0100

    E07 - Smooth Weights

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -37,7 +37,11 @@ public class Grid : MonoBehaviour
 
     //<layer Id , penalty> holder
     Dictionary<int, int> walkableRegionDictionary = new Dictionary<int, int>();
-    
+
+
+    int penaltyMin = int.MaxValue;
+    int penaltyMax = int.MinValue;
+    public int obstacleProximityPenalty = 10;
     #endregion
 
     private void Awake()
@@ -74,21 +78,95 @@ public class Grid : MonoBehaviour
                 //Calculate the movement penalty on each node
                 int movementPenalty = 0;
 
-                if (walkable)
+                Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100, walkableMask))
                 {
-                    Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
-                    RaycastHit hit;
-                    if(Physics.Raycast(ray, out hit, 100, walkableMask))
-                    {
-                        walkableRegionDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
-                    }
+                    walkableRegionDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
+                }
+
+                if (!walkable)
+                {
+                    movementPenalty += obstacleProximityPenalty;
                 }
 
                 grid[x, y] = new Node(walkable, worldPoint, x, y, movementPenalty);
             }
         }
 
+
+        BlurPenaltyMap(5);
     }
+
+
+    void BlurPenaltyMap(int blurSize)
+    {
+        //It is an `odd` number which is the center of the squre
+        int kernelSize = blurSize * 2 + 1;
+
+        //How many square between the center of the kernel to the edge
+        int kernelExtents = (kernelSize - 1) / 2;
+
+        int[,] penaltiesHorizontalPass = new int[gridSizeX, gridSizeY];
+        int[,] penaltiesVerticalPass = new int[gridSizeX, gridSizeY];
+
+
+        //calculate rows
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = -kernelExtents; x <= kernelExtents; x++)
+            {
+                int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                penaltiesHorizontalPass[0, y] += grid[sampleX, y].movementPenalty;
+            }
+
+            for (int x = 1; x < gridSizeX; x++)
+            {
+                int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
+                int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX - 1);
+
+                penaltiesHorizontalPass[x, y] = penaltiesHorizontalPass[x - 1, y] - grid[removeIndex, y].movementPenalty + grid[addIndex, y].movementPenalty;
+            }
+        }
+
+
+        //calculate columns
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = -kernelExtents; y <= kernelExtents; y++)
+            {
+                int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                penaltiesVerticalPass[x, 0] += penaltiesHorizontalPass[x, sampleY];
+            }
+
+            int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+            grid[x, 0].movementPenalty = blurredPenalty;
+
+            for (int y = 1; y < gridSizeY; y++)
+            {
+                int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
+                int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY - 1);
+
+                penaltiesVerticalPass[x, y] = penaltiesVerticalPass[x, y - 1] - penaltiesHorizontalPass[x, removeIndex] + penaltiesHorizontalPass[x, addIndex];
+                blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, y] / (kernelSize * kernelSize));
+                grid[x, y].movementPenalty = blurredPenalty;
+
+                //set min & max for drawing gizmos
+                if (blurredPenalty > penaltyMax)
+                {
+                    penaltyMax = blurredPenalty;
+                }
+
+                if (blurredPenalty < penaltyMin)
+                {
+                    penaltyMin = blurredPenalty;
+                }
+            }
+        }
+
+    }
+
+
 
     //Get the list of all 8 neigbours around the node
     public List<Node> GetNeigbours(Node node)
@@ -135,15 +213,17 @@ public class Grid : MonoBehaviour
     //Draw a wired cube to simulate the whole grid space
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        //Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
         if(grid != null && displayGridGizmos)
         {
             //Node playerNode = NodeFromWorldPoint(player.position);
             foreach (Node n in grid)
             {
-                Gizmos.color = (n.walkable) ? Color.white : Color.red;
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * nodeDiamater * 0.95f);
+                Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(penaltyMin, penaltyMax, n.movementPenalty));
+
+                Gizmos.color = (n.walkable) ? Gizmos.color : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * nodeDiamater);
             }
         }
     }
